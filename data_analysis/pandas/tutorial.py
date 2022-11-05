3aa05365cca56ab2be206d54caab2711cd803bed
przemys <czuba.przemyslaw@gmail.com>
2022-11-05 06:27:05 +0100

    micrograd part 2, pandas last part

--- data_analysis/pandas/tutorial.py
+++ data_analysis/pandas/tutorial.py
@@ -126,7 +126,23 @@ reviews.region_2.fillna("Unknown")  # replace NaN with "Unknown"
 reviews.taster_twitter_handle.replace("@kerinokeefe", "@kerino")  # replace value with value
 n_missing_prices = len(reviews[pd.isnull(reviews.price)])  # number of rows with missing values
 
+"""
+Renaming and Combining
+"""
 
+reviews.rename(columns={'points': 'score'})
+reviews.rename(index={0: 'firstEntry', 1: 'secondEntry'})
+reviews.rename_axis("wines", axis='rows').rename_axis("fields", axis='columns')
+
+canadian_youtube = pd.read_csv("../input/youtube-new/CAvideos.csv")
+british_youtube = pd.read_csv("../input/youtube-new/GBvideos.csv")
+
+pd.concat([canadian_youtube, british_youtube])
+
+left = canadian_youtube.set_index(['title', 'trending_date'])
+right = british_youtube.set_index(['title', 'trending_date'])
+
+left.join(right, lsuffix='_CAN', rsuffix='_UK')
 
 
 
