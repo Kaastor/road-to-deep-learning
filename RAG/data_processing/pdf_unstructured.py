db70798b176162cdee874c452ad9590fe98241b6
przemys <czuba.przemyslaw@gmail.com>
2023-10-16 08:46:19 +0200

    llama index tests

---
+++ RAG/data_processing/pdf_unstructured.py
@@ -0,0 +1,6 @@
+from unstructured.partition.pdf import partition_pdf
+
+elements = partition_pdf(filename="/home/przemek/GAT/knowledge-base/calendar/SUBSCRIBE USERS TO MULTIPLE GOOGLE CALENDARS IN BULK.pdf")
+elements2 = partition_pdf(filename="/home/przemek/GAT/knowledge-base/calendar/SEARCHING FOR GOOGLE CALENDARS.pdf")
+
+print()
